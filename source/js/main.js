5cedf6a3c571b588a1150bb341d48e41b1a16eb5
CirillSB <cirill2020@yandex.ru>
2023-04-13 14:14:43 +0700

    Убрал ошибки

--- source/js/main.js
+++ source/js/main.js
@@ -36,14 +36,11 @@ window.addEventListener('DOMContentLoaded', () => {
   showLogo();
   showMenu();
   initLoadImages();
+
   // ---------------------------------
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // initModals();
-    // const form = new Form();
-    // window.form = form;
-    // form.init();
     setFocusCard();
     showCards();
     setMaskTel();
