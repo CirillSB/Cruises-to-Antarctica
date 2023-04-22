2f2e27f13d0e4d4d00dd9cba557ae1099751f266
CirillSB <cirill2020@yandex.ru>
2023-04-22 21:13:21 +0700

    Переделал меню

--- source/js/main.js
+++ source/js/main.js
@@ -37,6 +37,107 @@ window.addEventListener('DOMContentLoaded', () => {
   showMenu();
   initLoadImages();
 
+
+
+
+  const initMobileMenu = () => {
+  if (window.matchMedia('(min-width: 1024px)').matches) {
+    const body = document.querySelector('body');
+    const page = document.querySelector('.wrapper');
+    const buttonOpenModal = document.querySelector('[data-open-modal]');
+    const buttonCloseModal = document.querySelectorAll('[data-close-modal]');
+    const modal = document.querySelector('[data-modal="feedback"]');
+    const nameInput = document.querySelector('#popup-name');
+    const focusableElements = document.querySelectorAll('[data-focusable-element]');
+    const firstFocusableElement = focusableElements[1];
+    const lastFocusableElement = focusableElements[0];
+    const KEYCODE_TAB = 9;
+
+    const createFocusNoose = () => {
+      modal.addEventListener('keydown', (e) => {
+        const isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+        if (!isTabPressed) {
+          return;
+        }
+
+        if (e.shiftKey) /* shift + tab */ {
+          if (document.activeElement === firstFocusableElement) {
+            lastFocusableElement.focus();
+            e.preventDefault();
+          }
+        } else /* tab */ {
+          if (document.activeElement === lastFocusableElement) {
+            firstFocusableElement.focus();
+            e.preventDefault();
+          }
+        }
+      });
+    };
+
+    const showModal = () => {
+      modal.classList.add('is-active');
+      buttonOpenModal.removeEventListener('click', showModal);
+      modal.addEventListener('click', hideModalClick);
+      document.addEventListener('keydown', hideModalKeyDown);
+      nameInput.focus();
+      body.style.overflowY = 'hidden';
+      page.inert = true;
+      createFocusNoose();
+    };
+
+    const closeModal = () => {
+      modal.classList.remove('is-active');
+      buttonOpenModal.addEventListener('click', showModal);
+      modal.removeEventListener('click', hideModalClick);
+      document.removeEventListener('keydown', hideModalKeyDown);
+      body.style.overflowY = 'visible';
+      page.inert = false;
+    };
+
+    const hideModalClick = () => {
+      for (let i = 0; i < buttonCloseModal.length; i++) {
+        if (event.target === buttonCloseModal[i]) {
+          closeModal();
+        }
+      }
+    };
+
+    const hideModalKeyDown = (e) => {
+      if (e.keyCode === 27) {
+        closeModal();
+      }
+    };
+
+    buttonOpenModal.addEventListener('click', showModal);
+
+
+  }
+};
+
+initMobileMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // ---------------------------------
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
--- source/js/modules/show-menu.js
+++ source/js/modules/show-menu.js
@@ -1,36 +1,56 @@
 export const showMenu = () => {
+  const body = document.querySelector('body');
+  const page = document.querySelector('.wrapper');
+  const modal = document.querySelector('[data-modal]');
+  const overlay = document.querySelector('.modal__overlay');
+
   const button = document.querySelector('.menu-button');
   const buttonContent = document.querySelector('.burger');
   const buttonImage = document.querySelector('.burger__image');
-  const menu = document.querySelector('.navigation__inner');
+  const menu = document.querySelector('.navigation--modal');
   const logo = document.querySelector('[data-header-logo]');
   const links = document.querySelectorAll('[data-link]');
 
-  const changeMenuVisibility = () => {
-    if (!menu.classList.contains('navigation__inner--js-active')) {
-      menu.classList.add('navigation__inner--js-active');
-      buttonImage.href.baseVal = 'img/sprite.svg#plus';
-      buttonContent.style.width = 15;
-      buttonContent.style.height = 15;
-      logo.style.opacity = 0;
-    } else {
-      menu.classList.remove('navigation__inner--js-active');
-      buttonImage.href.baseVal = 'img/sprite.svg#burger';
-      buttonContent.style.width = 24;
-      buttonContent.style.height = 12;
-      logo.style.opacity = 1;
-    }
-  };
-  const hideMenu = () => {
-    for (let i = 0; i < links.length; i++) {
-      if (event.target === links[i]) {
-        changeMenuVisibility();
-      }
-    }
+  const hideModal = () => {
+    modal.classList.remove('is-active');
+    body.classList.remove('scroll-lock-ios');
+
+    modal.removeEventListener('pointerdown', hideModalClick);
+    overlay.addEventListener('pointerdown', hideModal);
+
+    buttonImage.href.baseVal = 'img/sprite.svg#burger';
+    buttonContent.style.width = 24;
+    buttonContent.style.height = 12;
   };
 
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    button.addEventListener('pointerdown', changeMenuVisibility);
-    menu.addEventListener('pointerdown', hideMenu);
+  const hideModalClick = () => {
+    for (let i = 0; i < links.length; i++) {
+      if (event.target === links[i]) {
+        hideModal();
+      }
+    }
   }
-};
+
+  const showModal = () => {
+    modal.classList.add('is-active');
+    body.classList.add('scroll-lock-ios');
+
+    modal.addEventListener('pointerdown', hideModalClick);
+    overlay.addEventListener('pointerdown', hideModal);
+
+    buttonImage.href.baseVal = 'img/sprite.svg#plus';
+    buttonContent.style.width = 15;
+    buttonContent.style.height = 15;
+  }
+
+  const initModal = () => {
+    if (!modal.classList.contains('is-active')) {
+      showModal();
+    } else if (modal.classList.contains('is-active')) {
+      hideModal();
+    }
+  }
+  
+  button.addEventListener('pointerdown', initModal);
+
+}
